6d022ee3cb6f7c59180a2978d6edf90232d65666
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-03 13:59:50 -0700

    added const

---
+++ script.js
@@ -0,0 +1,6 @@
+//define the different sets
+
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?/~`-=';
