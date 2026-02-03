44f1ea4450db8cf61c64d2159f6ed6218cdc706a
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-03 14:02:27 -0700

    making function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-//define the different sets
+//define the different sets so it knows what we want  
 
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?/~`-=';
+
+
+//tell the computer how to do what we want
+function generatePassword(length, useLower, useUpper, useNumbers, useSymbols) {
