3d267dd5f996c26708cf2a09213bdbd4355cf6ab
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-03 14:10:01 -0700

    added divs for checkbox

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?/~`-=';
 
 //tell the computer how to do what we want
 function generatePassword(length, useLower, useUpper, useNumbers, useSymbols) {
+    let availableChars = '';
+
+if(useLower) availableChars += lowerCase;
+if(useUpper) availableChars += upperCase;
+if(useNumbers) availableChars += numbers;
+if(useSymbols) availableChars += specialChars;
+
+        if(availableChars.length === 0) {
+            return 'type something in'; //this is an error message
+        }
+
+let password = '';
+for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * availableChars.length);
+    password += availableChars[randomIndex]; //randomizes the password
+}
+
+return password;
+
+}
